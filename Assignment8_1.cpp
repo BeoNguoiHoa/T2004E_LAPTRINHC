a9c297875e6450a3b39757c7edb3128fbddb604d
BeoNguoiHoa <quanthanhtuc@gmail.com>
2020-05-23 18:14:37 +0700

    Assignment 8

---
+++ Assignment8_1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n,*p;
+	printf("nhap n=\n");
+	scanf("%d",&n);
+	p =(int *)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++){
+		printf("nhap pt thu %d:\n",i);
+		scanf("%d",p+i);
+	}
+	int max = 0, count = 0;
+	for(int i=0;i<n;i++){
+		if(*(p+i) > 0){
+			count++;
+			if(count > max) max = count;
+		}else{
+			count = 0;
+		}
+	}
+	printf("so luong so duong lien tiep nhieu nhat : %d",max);
+	
+}
